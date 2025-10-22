adc6c8432fbf8d261ec49c6edc4154823ed40486
lugonpa10 <lugonpa10@gmail.com>
2025-10-22 13:04:42 +0200

    changes

--- TEMA 3/jsonproyect/src/main/java/json/Main.java
+++ TEMA 3/jsonproyect/src/main/java/json/Main.java
@@ -1,7 +1,67 @@
 package json;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.json.Json;
+import javax.json.JsonReader;
+import javax.json.JsonValue;
+import javax.net.ssl.HttpsURLConnection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       System.out.println(leeJSON("https://pokeapi.co/"));
+    }
+
+    public static JsonValue leeJSON(String ruta) {
+        try {
+            if (ruta.toLowerCase().startsWith("http://")) {
+                return leerHttp(ruta);
+            } else if (ruta.toLowerCase().startsWith("https://")) {
+                return leerHttps(ruta); 
+            } else {
+                return leerFichero(ruta);
+            }
+        } catch (IOException e) {
+            System.out.println("Error procesando documento Json " +
+                    e.getLocalizedMessage());
+            return null;
+        }
+    }
+
+    public static JsonValue leerFichero(String ruta) throws FileNotFoundException {
+        try (JsonReader reader = Json.createReader(new FileReader(ruta))) {
+            return reader.read();
+            /*
+             * JsonStructure jsonSt = reader.read();
+             * System.out.println(jsonSt.getValueType());
+             * JsonObject jsonObj = reader.readObject();
+             * System.out.println(jsonObj.getValueType());
+             * JsonArray jsonArr = reader.readArray();
+             * System.out.println(jsonArr.getValueType());
+             */
+        }
+    }
+
+    public static JsonValue leerHttp(String direccion) throws IOException {
+        URL url = new URL(direccion);
+        try (InputStream is = url.openStream();
+                JsonReader reader = Json.createReader(is)) {
+            return reader.read();
+        }
+    }
+
+    public static JsonValue leerHttps(String direccion) throws IOException {
+        URL url = new URL(direccion);
+        HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+        try (InputStream is = conn.getInputStream();
+                JsonReader reader = Json.createReader(is)) {
+            return reader.read();
+        } finally {
+            conn.disconnect();
+        }
     }
 }
