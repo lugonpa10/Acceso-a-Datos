529b1ee34dfdf5e85fbe5c6102457826fdb2805d
lugonpa10 <lugonpa10@gmail.com>
2026-01-14 13:16:01 +0100

    changes

--- TEMA 5/tema5maven/src/main/java/ejem1/GestionaPersona.java
+++ TEMA 5/tema5maven/src/main/java/ejem1/GestionaPersona.java
@@ -4,11 +4,10 @@ import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/personas")
+@Path("/persona")
 public class GestionaPersona {
     static Persona p;
 
--- TEMA 5/tema5maven/src/main/java/ejem1/Personas.java
+++ TEMA 5/tema5maven/src/main/java/ejem1/Personas.java
@@ -6,6 +6,7 @@ import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
@@ -14,25 +15,36 @@ public class Personas {
     static ArrayList<Persona> personas = new ArrayList<>();
 
     @POST
-    @Consumes({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
-    public void guardar(Persona p){
-       
-       
+    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public void guardar(Persona p) {
+
         this.personas.add(p);
-        
+
     }
 
-@GET
-@Produces({MediaType.APPLICATION_JSON})
-public ArrayList<Persona> listar(){
-    return personas;
+    @GET
+    @Produces({ MediaType.APPLICATION_XML })
 
-}
+    public ArrayList<Persona> listar() {
+        return personas;
+    }
 
+    @GET
+    @Produces({ MediaType.APPLICATION_JSON })
+    @Path("/{nombre}")
+    public Persona ver(@PathParam("nombre") String nombre) {
+        for (Persona p : personas) {
+            if (p.getNombre().equals(nombre)) {
+                return p;
 
+            }
 
+        }
+        return null;
 
+    }
 
+    // @GET
+    // @Path("/buscar")
 
-    
 }
