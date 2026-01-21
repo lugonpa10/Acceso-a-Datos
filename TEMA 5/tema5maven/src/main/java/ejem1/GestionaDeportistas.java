a20a3966a1d4b6c95c3209ad048fd0e821e03d6e
lugonpa10 <lugonpa10@gmail.com>
2026-01-21 13:14:56 +0100

    changes

--- TEMA 5/tema5maven/src/main/java/ejem1/GestionaDeportistas.java
+++ TEMA 5/tema5maven/src/main/java/ejem1/GestionaDeportistas.java
@@ -8,8 +8,11 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.mariadb.jdbc.export.ExceptionFactory.SqlExceptionFactory;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.GenericEntity;
 import jakarta.ws.rs.core.MediaType;
@@ -21,19 +24,20 @@ public class GestionaDeportistas {
     private static final String URL = "jdbc:mariadb://localhost:3306/ad_tema6";
     private static final String USER = "root";
     private static final String PASS = "";
+    public static ArrayList<Deportista> listaDeportistas = new ArrayList<>();
+    Deportista deportista;
 
     @GET
     @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
     public Response obtenerTodos() {
-        ArrayList<Deportista> listaDeportistas = new ArrayList<>();
 
         try {
 
             Class.forName("org.mariadb.jdbc.Driver");
 
-            try (Connection conexion = DriverManager.getConnection(URL, USER, PASS);
-                    Statement st = conexion.createStatement();
-                    ResultSet rs = st.executeQuery("Select * from deportistas");) {
+            try (Connection conexion = DriverManager.getConnection(URL, USER, PASS)) {
+                Statement st = conexion.createStatement();
+                ResultSet rs = st.executeQuery("Select * from deportistas");
                 while (rs.next()) {
                     listaDeportistas.add(new Deportista(rs.getInt("id"),
                             rs.getString("nombre"),
@@ -57,5 +61,171 @@ public class GestionaDeportistas {
         }
 
     }
+
+    @Path("/{id}")
+    @GET
+    @Produces({ MediaType.APPLICATION_JSON })
+    public Response buscarJugador(@PathParam("id") String id) {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+
+            try (Connection conexion = DriverManager.getConnection(URL, USER, PASS)) {
+                Statement st = conexion.createStatement();
+                ResultSet rs = st.executeQuery("Select * from deportistas where id = " + id);
+                while (rs.next()) {
+                    deportista = new Deportista(rs.getInt("id"),
+                            rs.getString("nombre"),
+                            rs.getBoolean("activo"),
+                            rs.getString("deporte"),
+                            rs.getString("genero"));
+
+                }
+                return Response.ok(deportista).build();
+
+            } catch (SQLException e) {
+                return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Error SQL")
+                        .build();
+            }
+
+        } catch (ClassNotFoundException e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("No se encuentra el driver").build();
+
+        }
+
+    }
+
+    @Path("/deporte/{nombreDeporte}")
+    @GET
+    @Produces({ MediaType.APPLICATION_JSON })
+    public Response listarPorDeporte(@PathParam("nombreDeporte") String nombreDeporte) {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            try (Connection conexion = DriverManager.getConnection(URL, USER, PASS)) {
+                Statement st = conexion.createStatement();
+              ResultSet rs = st.executeQuery("SELECT * FROM deportistas WHERE deporte = '" + nombreDeporte + "'");
+                while (rs.next()) {
+                    listaDeportistas.add(new Deportista(rs.getInt("id"),
+                            rs.getString("nombre"),
+                            rs.getBoolean("activo"),
+                            rs.getString("deporte"),
+                            rs.getString("genero")));
+
+                }
+                return Response.ok(listaDeportistas).build();
+
+            } catch (SQLException e) {
+                return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Error SQL")
+                        .build();
+            }
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("No se encuentra el driver").build();
+           
+        }
+
+    }
+
+    @Path("/activos")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response activos(){
+        try {
+
+            Class.forName("org.mariadb.jdbc.Driver");
+
+            try (Connection conexion = DriverManager.getConnection(URL, USER, PASS)) {
+                Statement st = conexion.createStatement();
+                ResultSet rs = st.executeQuery("Select * from deportistas where activo = 1");
+                while (rs.next()) {
+                    listaDeportistas.add(new Deportista(rs.getInt("id"),
+                            rs.getString("nombre"),
+                            rs.getBoolean("activo"),
+                            rs.getString("deporte"),
+                            rs.getString("genero")));
+                }
+
+                GenericEntity<List<Deportista>> entity = new GenericEntity<List<Deportista>>(listaDeportistas) {
+                };
+
+                return Response.ok(entity).build();
+
+            } catch (SQLException e) {
+                return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Error SQL")
+                        .build();
+
+            }
+        } catch (ClassNotFoundException e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("No se encuentra el driver").build();
+        }
+    }
+
+    @Path("/retirados")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response retirados(){
+        try {
+
+            Class.forName("org.mariadb.jdbc.Driver");
+
+            try (Connection conexion = DriverManager.getConnection(URL, USER, PASS)) {
+                Statement st = conexion.createStatement();
+                ResultSet rs = st.executeQuery("Select * from deportistas where activo = 0");
+                while (rs.next()) {
+                    listaDeportistas.add(new Deportista(rs.getInt("id"),
+                            rs.getString("nombre"),
+                            rs.getBoolean("activo"),
+                            rs.getString("deporte"),
+                            rs.getString("genero")));
+                }
+
+                GenericEntity<List<Deportista>> entity = new GenericEntity<List<Deportista>>(listaDeportistas) {
+                };
+
+                return Response.ok(entity).build();
+
+            } catch (SQLException e) {
+                return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Error SQL")
+                        .build();
+
+            }
+        } catch (ClassNotFoundException e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("No se encuentra el driver").build();
+        }
+    }
+
+     @Path("/masculinos")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response masculinos(){
+        try {
+
+            Class.forName("org.mariadb.jdbc.Driver");
+
+            try (Connection conexion = DriverManager.getConnection(URL, USER, PASS)) {
+                Statement st = conexion.createStatement();
+                ResultSet rs = st.executeQuery("Select * from deportistas where genero=");
+                while (rs.next()) {
+                    listaDeportistas.add(new Deportista(rs.getInt("id"),
+                            rs.getString("nombre"),
+                            rs.getBoolean("activo"),
+                            rs.getString("deporte"),
+                            rs.getString("genero")));
+                }
+
+                GenericEntity<List<Deportista>> entity = new GenericEntity<List<Deportista>>(listaDeportistas) {
+                };
+
+                return Response.ok(entity).build();
+
+            } catch (SQLException e) {
+                return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Error SQL")
+                        .build();
+
+            }
+        } catch (ClassNotFoundException e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("No se encuentra el driver").build();
+        }
+    }
+
+
 
 }
